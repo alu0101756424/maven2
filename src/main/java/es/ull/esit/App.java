6897d32a1262c99e4cfa1389d9e9f2b9ff612db9
Aitor Hermida <alu0101756424@ull.edu.es>
2025-10-30 12:35:59 +0000

    zero

--- src/main/java/es/ull/esit/App.java
+++ src/main/java/es/ull/esit/App.java
@@ -6,5 +6,10 @@ package es.ull.esit;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int sum = 0;
+        int n = 10;
+        n=n/0;
+        System.out.println("n="+n);
+        }
     }
 }
