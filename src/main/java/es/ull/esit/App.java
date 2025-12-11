5b7a551f19ff926b3a8807cf457a4c94f3f9bc60
Aitor Manuel Perdomo Hermida <alu0101756424@ull.edu.es>
2025-12-11 11:56:11 +0000

    Remove division by zero from main method
    Removed division by zero and related variables.

--- src/main/java/es/ull/esit/App.java
+++ src/main/java/es/ull/esit/App.java
@@ -6,10 +6,6 @@ package es.ull.esit;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int sum = 0;
-        int n = 10;
-        n=n/0;
-        System.out.println("n="+n);
         }
     }
 }
